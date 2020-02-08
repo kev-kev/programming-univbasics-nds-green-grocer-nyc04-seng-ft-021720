c818d6489eeadf3950dd87949f220b4be2f46b60
Kevin Clark <kevin.clark93@gmail.com>
2020-02-08 00:46:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,7 +39,7 @@ def apply_coupons(cart, coupons)
   cart.each { |grocery|
     coupons.each{ |coupon|
     # looking for a way to move onto the next coupon if it there aren't enough items to apply the coupon
-    if grocery[:count] - coupons[:num] < 0
+    if grocery[:count] - coupon[:num] < 0
       break
     elsif coupon[:item] == grocery[:item]
       # Subtracting the items the coupon will be applied to
